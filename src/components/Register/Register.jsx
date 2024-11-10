cf15e81076b58dfa877def3aef4b00fed415a5a4
sayak53 <sayakchakraborty928@gmail.com>
2024-11-10 13:36:20 +0530

    basic set up done

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,11 +1,73 @@
-import React from 'react';
+import React from "react";
 
 const Register = () => {
-    return (
-        <div>
-            register kor bara
+    const handlesubmit = (e) => {
+       e.preventDefault()
+       console.log('dbzvjbvd')
+    }
+  return (
+    <div className="w-[50%] mx-auto">
+      <form className="card-body">
+
+      <div className="form-control">
+          <label className="label">
+            <span className="label-text">First Name</span>
+          </label>
+          <input
+            type="text"
+            placeholder="Enter Your First Name"
+            className="input input-bordered"
+            required
+          />
         </div>
-    );
+
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Last Name</span>
+          </label>
+          <input
+            type="text"
+            placeholder="Enter Your Last Name"
+            className="input input-bordered"
+            required
+          />
+        </div>
+
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Email</span>
+          </label>
+          <input
+            type="email"
+            placeholder="Enter Your Email"
+            className="input input-bordered"
+            required
+          />
+        </div>
+
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Password</span>
+          </label>
+          <input
+            type="password"
+            placeholder="password"
+            className="input input-bordered"
+            required
+          />
+          <label className="label">
+            <a href="#" className="label-text-alt link link-hover">
+              Forgot password?
+            </a>
+          </label>
+        </div>
+
+        <div className="form-control mt-6">
+          <button onClick={handlesubmit} className="btn btn-primary">Login</button>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default Register;
